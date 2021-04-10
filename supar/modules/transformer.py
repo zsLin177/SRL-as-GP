4452b7bc49adf0c2d1d99764f114fcb9634ab306
yzhangcs <yzhang.cs@outlook.com>
2021-04-10 22:41:34 +0800

    Different pooling ways

--- supar/modules/transformer.py
+++ supar/modules/transformer.py
@@ -24,6 +24,10 @@ class TransformerEmbedding(nn.Module):
         stride (int):
             A sequence longer than max length will be splitted into several small pieces
             with a window size of ``stride``. Default: 10.
+        pooling (str):
+            Pooling way to get from token piece embeddings to token embedding.
+            Either take the first subtoken ('first'), the last subtoken ('last'), or a mean over all ('mean').
+            Default: 'mean'.
         pad_index (int):
             The index of the padding token in the BERT vocabulary. Default: 0.
         dropout (float):
@@ -37,7 +41,7 @@ class TransformerEmbedding(nn.Module):
         https://github.com/huggingface/transformers
     """
 
-    def __init__(self, model, n_layers, n_out=0, stride=10, pad_index=0, dropout=0, requires_grad=False):
+    def __init__(self, model, n_layers, n_out=0, stride=10, pooling='mean', pad_index=0, dropout=0, requires_grad=False):
         super().__init__()
 
         from transformers import AutoConfig, AutoModel, AutoTokenizer
@@ -49,6 +53,7 @@ class TransformerEmbedding(nn.Module):
         self.hidden_size = self.bert.config.hidden_size
         self.n_out = n_out or self.hidden_size
         self.stride = stride
+        self.pooling = pooling
         self.pad_index = pad_index
         self.dropout = dropout
         self.requires_grad = requires_grad
@@ -60,7 +65,8 @@ class TransformerEmbedding(nn.Module):
         self.projection = nn.Linear(self.hidden_size, self.n_out, False) if self.hidden_size != n_out else nn.Identity()
 
     def __repr__(self):
-        s = f"{self.model}, n_layers={self.n_layers}, n_out={self.n_out}, pad_index={self.pad_index}"
+        s = f"{self.model}, n_layers={self.n_layers}, n_out={self.n_out}, "
+        s += f"stride={self.stride}, pooling={self.pooling}, pad_index={self.pad_index}"
         if self.dropout > 0:
             s += f", dropout={self.dropout}"
         if self.requires_grad:
@@ -76,7 +82,7 @@ class TransformerEmbedding(nn.Module):
             ~torch.Tensor:
                 BERT embeddings of shape ``[batch_size, seq_len, n_out]``.
         """
-        batch_size, seq_len, fix_len = subwords.shape
+
         mask = subwords.ne(self.pad_index)
         lens = mask.sum((1, 2))
         # [batch_size, n_subwords]
@@ -94,13 +100,18 @@ class TransformerEmbedding(nn.Module):
             part = self.bert(subwords[:, i:i+self.max_len], attention_mask=bert_mask[:, i:i+self.max_len].float())[-1]
             bert = torch.cat((bert, self.scalar_mix(part[-self.n_layers:])[:, self.max_len-self.stride:]), 1)
 
-        # [batch_size, n_subwords]
+        # [batch_size, seq_len]
         bert_lens = mask.sum(-1)
         bert_lens = bert_lens.masked_fill_(bert_lens.eq(0), 1)
         # [batch_size, seq_len, fix_len, hidden_size]
         embed = bert.new_zeros(*mask.shape, self.hidden_size).masked_scatter_(mask.unsqueeze(-1), bert[bert_mask])
         # [batch_size, seq_len, hidden_size]
-        embed = embed.sum(2) / bert_lens.unsqueeze(-1)
+        if self.pooling == 'first':
+            embed = embed[:, :, 0]
+        elif self.pooling == 'last':
+            embed = embed.gather(2, (bert_lens-1).unsqueeze(-1).repeat(1, 1, self.hidden_size).unsqueeze(2)).squeeze(2)
+        else:
+            embed = embed.sum(2) / bert_lens.unsqueeze(-1)
         embed = self.projection(embed)
 
         return embed
