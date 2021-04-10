be4e78bb27abfc3b083683ce2e7a819cf4d280a1
yzhangcs <yzhang.cs@outlook.com>
2021-04-10 11:07:13 +0800

    Update links

--- supar/__init__.py
+++ supar/__init__.py
@@ -25,27 +25,43 @@ PARSER = {parser.NAME: parser for parser in [BiaffineDependencyParser,
                                              VISemanticDependencyParser]}
 
 MODEL = {
-    'biaffine-dep-lstm-char': 'http://hlt.suda.edu.cn/LA/yzhang/supar/ptb.biaffine.dep.lstm.char',
+    'biaffine-dep-lstm-char-en': 'http://hlt.suda.edu.cn/LA/yzhang/supar/ptb.biaffine.dep.lstm.char',
     'biaffine-dep-lstm-char-zh': 'http://hlt.suda.edu.cn/LA/yzhang/supar/ctb7.biaffine.dep.lstm.char',
-    'crf-con-lstm-char': 'http://hlt.suda.edu.cn/LA/yzhang/supar/ptb.crf.con.lstm.char',
-    'crf-con-lstm-char-zh': 'http://hlt.suda.edu.cn/LA/yzhang/supar/ctb7.crf.con.lstm.char',
-    'biaffine-dep-roberta': 'http://hlt.suda.edu.cn/LA/yzhang/supar/ptb.biaffine.dep.roberta',
+    'crf-dep-lstm-char-en': 'http://hlt.suda.edu.cn/LA/yzhang/supar/ptb.crf.dep.lstm.char',
+    'crf-dep-lstm-char-zh': 'http://hlt.suda.edu.cn/LA/yzhang/supar/ctb7.crf.dep.lstm.char',
+    'crf2o-dep-lstm-char-en': 'http://hlt.suda.edu.cn/LA/yzhang/supar/ptb.crf2o.dep.lstm.char',
+    'crf2o-dep-lstm-char-zh': 'http://hlt.suda.edu.cn/LA/yzhang/supar/ctb7.crf2o.dep.lstm.char',
+    'vi-dep-lstm-char-en': 'http://hlt.suda.edu.cn/LA/yzhang/supar/ptb.vi.dep.lstm.char',
+    'vi-dep-lstm-char-zh': 'http://hlt.suda.edu.cn/LA/yzhang/supar/ctb7.vi.dep.lstm.char',
+    'biaffine-dep-roberta-en': 'http://hlt.suda.edu.cn/LA/yzhang/supar/ptb.biaffine.dep.roberta',
     'biaffine-dep-electra-zh': 'http://hlt.suda.edu.cn/LA/yzhang/supar/ctb7.biaffine.dep.electra',
-    'crf-con-roberta': 'http://hlt.suda.edu.cn/LA/yzhang/supar/ptb.crf.con.roberta',
+    'crf-con-lstm-char-en': 'http://hlt.suda.edu.cn/LA/yzhang/supar/ptb.crf.con.lstm.char',
+    'crf-con-lstm-char-zh': 'http://hlt.suda.edu.cn/LA/yzhang/supar/ctb7.crf.con.lstm.char',
+    'vi-con-lstm-char-en': 'http://hlt.suda.edu.cn/LA/yzhang/supar/ptb.vi.con.lstm.char',
+    'vi-con-lstm-char-zh': 'http://hlt.suda.edu.cn/LA/yzhang/supar/ctb7.vi.con.lstm.char',
+    'crf-con-roberta-en': 'http://hlt.suda.edu.cn/LA/yzhang/supar/ptb.crf.con.roberta',
     'crf-con-electra-zh': 'http://hlt.suda.edu.cn/LA/yzhang/supar/ctb7.crf.con.electra',
-    'biaffine-sdp': 'http://hlt.suda.edu.cn/LA/yzhang/supar/dm.biaffine.sdp.lstm.tag-char-lemma',
-    'vi-sdp': 'http://hlt.suda.edu.cn/LA/yzhang/supar/dm.vi.sdp.lstm.tag-char-lemma'
+    'biaffine-sdp-en': 'http://hlt.suda.edu.cn/LA/yzhang/supar/dm.biaffine.sdp.lstm.tag-char-lemma',
+    'vi-sdp-en': 'http://hlt.suda.edu.cn/LA/yzhang/supar/dm.vi.sdp.lstm.tag-char-lemma'
 }
 
 CONFIG = {
-    'biaffine-dep-lstm-char': 'http://hlt.suda.edu.cn/LA/yzhang/supar/ptb.biaffine.dep.lstm.char.ini',
+    'biaffine-dep-lstm-char-en': 'http://hlt.suda.edu.cn/LA/yzhang/supar/ptb.biaffine.dep.lstm.char.ini',
     'biaffine-dep-lstm-char-zh': 'http://hlt.suda.edu.cn/LA/yzhang/supar/ctb7.biaffine.dep.lstm.char.ini',
-    'crf-con-lstm-char': 'http://hlt.suda.edu.cn/LA/yzhang/supar/ptb.crf.con.lstm.char.ini',
-    'crf-con-lstm-char-zh': 'http://hlt.suda.edu.cn/LA/yzhang/supar/ctb7.crf.con.lstm.char.ini',
-    'biaffine-dep-roberta': 'http://hlt.suda.edu.cn/LA/yzhang/supar/ptb.biaffine.dep.roberta.ini',
+    'crf-dep-lstm-char-en': 'http://hlt.suda.edu.cn/LA/yzhang/supar/ptb.crf.dep.lstm.char.ini',
+    'crf-dep-lstm-char-zh': 'http://hlt.suda.edu.cn/LA/yzhang/supar/ctb7.crf.dep.lstm.char.ini',
+    'crf2o-dep-lstm-char-en': 'http://hlt.suda.edu.cn/LA/yzhang/supar/ptb.crf2o.dep.lstm.char.ini',
+    'crf2o-dep-lstm-char-zh': 'http://hlt.suda.edu.cn/LA/yzhang/supar/ctb7.crf2o.dep.lstm.char.ini',
+    'vi-dep-lstm-char-en': 'http://hlt.suda.edu.cn/LA/yzhang/supar/ptb.vi.dep.lstm.char.ini',
+    'vi-dep-lstm-char-zh': 'http://hlt.suda.edu.cn/LA/yzhang/supar/ctb7.vi.dep.lstm.char.ini',
+    'biaffine-dep-roberta-en': 'http://hlt.suda.edu.cn/LA/yzhang/supar/ptb.biaffine.dep.roberta.ini',
     'biaffine-dep-electra-zh': 'http://hlt.suda.edu.cn/LA/yzhang/supar/ctb7.biaffine.dep.electra.ini',
-    'crf-con-roberta': 'http://hlt.suda.edu.cn/LA/yzhang/supar/ptb.crf.con.roberta.ini',
+    'crf-con-lstm-char-en': 'http://hlt.suda.edu.cn/LA/yzhang/supar/ptb.crf.con.lstm.char.ini',
+    'crf-con-lstm-char-zh': 'http://hlt.suda.edu.cn/LA/yzhang/supar/ctb7.crf.con.lstm.char.ini',
+    'vi-con-lstm-char-en': 'http://hlt.suda.edu.cn/LA/yzhang/supar/ptb.vi.con.lstm.char.ini',
+    'vi-con-lstm-char-zh': 'http://hlt.suda.edu.cn/LA/yzhang/supar/ctb7.vi.con.lstm.char.ini',
+    'crf-con-roberta-en': 'http://hlt.suda.edu.cn/LA/yzhang/supar/ptb.crf.con.roberta.ini',
     'crf-con-electra-zh': 'http://hlt.suda.edu.cn/LA/yzhang/supar/ctb7.crf.con.electra.ini',
-    'biaffine-sdp': 'http://hlt.suda.edu.cn/LA/yzhang/supar/dm.biaffine.sdp.lstm.tag-char-lemma.ini',
-    'vi-sdp': 'http://hlt.suda.edu.cn/LA/yzhang/supar/dm.vi.sdp.lstm.tag-char-lemma.ini'
+    'biaffine-sdp-en': 'http://hlt.suda.edu.cn/LA/yzhang/supar/dm.biaffine.sdp.lstm.tag-char-lemma.ini',
+    'vi-sdp-en': 'http://hlt.suda.edu.cn/LA/yzhang/supar/dm.vi.sdp.lstm.tag-char-lemm.ini'
 }
