56c186329aef06273ebb2093978521ae4eed1a7d
yzhangcs <yzhang.cs@outlook.com>
2021-04-12 12:31:41 +0800

    Minor fix

--- tests/test_parse.py
+++ tests/test_parse.py
@@ -1,12 +1,16 @@
 # -*- coding: utf-8 -*-
 
+import os
+
 import supar
+import torch
 from supar import Parser
 
 
 def test_parse():
     sentence = ['The', 'dog', 'chases', 'the', 'cat', '.']
     for name in supar.MODEL:
-        parser = Parser.load(name)
+        parser = Parser.load(name, device='cuda:0')
         parser.predict(sentence, prob=True)
         parser.predict(' '.join(sentence), prob=True, lang=('zh' if name.endswith('zh') else 'en'))
+        os.remove(os.path.join(torch.hub.get_dir(), 'checkpoints', os.path.basename(supar.MODEL[name])))
